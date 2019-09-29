0a3e7d3015d64035d772c02a9ee50b42ef0a8843
ManliestBen <benjamintmanley@gmail.com>
2019-09-29 12:49:24 -0500

    add site basic frame

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import '../node_modules/bootstrap-css-only/css/bootstrap.min.css'
 
 import '../node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css'
 import '../node_modules/mdbreact/dist/css/mdb.css';
 import './index.css';
-import App from './App';
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+
 
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render( <App /> , document.getElementById('root'));
 
-registerServiceWorker();
+class App extends Component {
+    state = {};
+    render() {
+    return (
+        <React.Fragment>
+        <MDBContainer>
+            <MDBRow>
+            <MDBCol lg="6">Left column</MDBCol>
+            <MDBCol lg="6">Right column</MDBCol>
+            </MDBRow>
+        </MDBContainer>
+        </React.Fragment>
+    );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
