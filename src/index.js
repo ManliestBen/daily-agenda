f754b4cc7d09fd4c2445629fd640fcb3bea564e4
ManliestBen <benjamintmanley@gmail.com>
2019-09-29 13:18:25 -0500

    add ability to add event

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ class App extends Component {
     constructor(props) {
       super(props);
       this.state = {
+        modal: false,
         events: [
           {
             id: 1,
@@ -41,9 +42,41 @@ class App extends Component {
         ]
       };
     }
+    addEvent = () => {
+      var newArray = [...this.state.events];
+      newArray.push({
+        id: newArray.length ? newArray[newArray.length - 1].id + 1 : 1,
+        time: this.state.time,
+        title: this.state.title,
+        location: this.state.location,
+        description: this.state.description,
+        value: this.var > 5 ? "Its's grater then 5" : "Its lower or equal 5"
+      });
+      this.setState({ events: newArray });
+      this.setState({
+        time: "",
+        title: "",
+        location: "",
+        description: ""
+      });
+    };
+  
+    handleInputChange = inputName => value => {
+      const nextValue = value;
+  
+      this.setState({
+        [inputName]: nextValue
+      });
+    };
+  
     handleDelete = eventId => {
       const events = this.state.events.filter(e => e.id !== eventId);
       this.setState({ events });
+    };
+    toggleModal = () => {
+      this.setState({
+        modal: !this.state.modal
+      });
     };
   
     render() {
@@ -68,7 +101,7 @@ class App extends Component {
                 </div>
                 <MDBRow className="mb-4">
                   <MDBCol xl="3" md="6" className="mx-auto text-center">
-                    <MDBBtn color="info" rounded>
+                    <MDBBtn color="info" rounded onClick={this.toggleModal}>
                       Add Event
                     </MDBBtn>
                   </MDBCol>
@@ -102,6 +135,65 @@ class App extends Component {
               </MDBCol>
             </MDBRow>
           </MDBContainer>
+  
+          <MDBModal isOpen={this.state.modal} toggle={this.toggleModal}>
+            <MDBModalHeader
+              className="text-center"
+              titleClass="w-100 font-weight-bold"
+              toggle={this.toggleModal}
+            >
+              Add new event
+            </MDBModalHeader>
+            <MDBModalBody>
+              <form className="mx-3 grey-text">
+                <MDBInput
+                  name="time"
+                  label="Time"
+                  icon="clock"
+                  hint="12:30"
+                  group
+                  type="text"
+                  getValue={this.handleInputChange("time")}
+                />
+                <MDBInput
+                  name="title"
+                  label="Title"
+                  icon="edit"
+                  hint="Briefing"
+                  group
+                  type="text"
+                  getValue={this.handleInputChange("title")}
+                />
+                <MDBInput
+                  name="location"
+                  label="Location (optional)"
+                  icon="map"
+                  group
+                  type="text"
+                  getValue={this.handleInputChange("location")}
+                />
+                <MDBInput
+                  name="description"
+                  label="Description (optional)"
+                  icon="sticky-note"
+                  group
+                  type="textarea"
+                  getValue={this.handleInputChange("description")}
+                />
+              </form>
+            </MDBModalBody>
+            <MDBModalFooter className="justify-content-center">
+              <MDBBtn
+                color="info"
+                onClick={() => {
+                  this.toggleModal();
+                  this.addEvent();
+                }}
+              >
+                Add
+              </MDBBtn>
+            </MDBModalFooter>
+          </MDBModal>
         </React.Fragment>
       );
     }
